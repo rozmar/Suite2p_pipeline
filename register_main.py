97f81cbdd45abfd2de9dbc254f1ffbde1574d2d3
rozmar <qtyush@gmail.com>
2022-05-14 23:56:39 -0700

    registering script works, no concatenation so far

--- register_main.py
+++ register_main.py
@@ -6,32 +6,46 @@ import numpy as np
 import threading
 from pathlib import Path
 import os
+import json
+import time
 import shutil
-from utils import utils_imaging, utils_io
+from utils import utils_io
 import sys
 
+local_temp_dir = sys.argv[1]#'/mnt/HDDS/Fast_disk_0/temp/'
+metadata_dir = sys.argv[2]#'/mnt/Data/BCI_metadata/'
+raw_scanimage_dir_base = sys.argv[3]#'/home/rozmar/Network/GoogleServices/BCI_data/Data/Calcium_imaging/raw/'
+suite2p_dir_base = sys.argv[4]#'/home/rozmar/Network/GoogleServices/BCI_data/Data/Calcium_imaging/suite2p/'
+try:
+    subject = sys.argv[5]#'BCI_29'
+except:
+    subject = None
+try:
+    setup = sys.argv[6]#'Bergamo-2P-Photostim'
+except:
+    setup = None
+
 # =============================================================================
-# local_temp_dir = sys.argv[1]#'/mnt/HDDS/Fast_disk_0/temp/'
-# metadata_dir = sys.argv[2]#'/mnt/Data/BCI_metadata/'
-# raw_scanimage_dir_base = sys.argv[3]#'/home/rozmar/Network/GoogleServices/BCI_data/Data/Calcium_imaging/raw/'
-# suite2p_dir_base = sys.argv[4]#'/home/rozmar/Network/GoogleServices/BCI_data/Data/Calcium_imaging/suite2p/'
-# try:
-#     subject = sys.argv[5]#'BCI_29'
-# except:
-#     subject = None
-# try:
-#     setup = sys.argv[6]#'Bergamo-2P-Photostim'
-# except:
-#     setup = None
+# local_temp_dir = '/mnt/HDDS/Fast_disk_0/temp/'
+# metadata_dir = '/mnt/Data/BCI_metadata/'
+# raw_scanimage_dir_base = '/home/rozmar/Network/GoogleServices/BCI_data/Data/Calcium_imaging/raw/'
+# suite2p_dir_base = '/home/rozmar/Network/GoogleServices/BCI_data/Data/Calcium_imaging/suite2p/'
+# subject = 'BCI_29'
+# setup = 'Bergamo-2P-Photostim'
 # =============================================================================
 
-local_temp_dir = '/mnt/HDDS/Fast_disk_0/temp/'
-metadata_dir = '/mnt/Data/BCI_metadata/'
-raw_scanimage_dir_base = '/home/rozmar/Network/GoogleServices/BCI_data/Data/Calcium_imaging/raw/'
-suite2p_dir_base = '/home/rozmar/Network/GoogleServices/BCI_data/Data/Calcium_imaging/suite2p/'
-subject = 'BCI_29'
-setup = 'Bergamo-2P-Photostim'
-
+s2p_params = {'max_reg_shift':50, # microns
+            'max_reg_shift_NR': 20, # microns
+            'block_size': 200, # microns
+            'smooth_sigma':0.5, # microns
+            'smooth_sigma_time':0, #seconds,
+            'overwrite': False,
+            #'num_workers':4,
+            'z_stack_name':'',
+            'reference_session':''} # folder where the suite2p output is saved
+trial_number_for_mean_image = 10
+processes_running = 0
+max_process_num = 5
 #% metadata will be updated manually, this script will read .csv files
 subject_metadata = pd.read_csv(os.path.join(metadata_dir,subject.replace('_','')+'.csv'))
 #decode session dates here
@@ -63,6 +77,7 @@ for FOV in FOV_list_:
         FOV_list.append(FOV)
 
 for FOV in FOV_list:
+    first_session_of_FOV = True
     session_dates = subject_metadata.loc[subject_metadata['FOV']==FOV,'Date']
     training_types = subject_metadata.loc[subject_metadata['FOV']==FOV,'Training type']
     for session_date,training_type in zip(session_dates,training_types):
@@ -79,8 +94,95 @@ for FOV in FOV_list:
         temp_movie_directory = os.path.join(local_temp_dir,'{}_{}'.format(subject,session))
         copy_thread = threading.Thread(target = utils_io.copy_tiff_files_in_order, args = (source_movie_directory,temp_movie_directory))
         copy_thread.start()
-        #utils_io.copy_tiff_files_in_order(source_movie_directory,temp_movie_directory) # this script copies all the data locally
-        asd
+        #% select the first Z-stack in the FOV directory
+        available_z_stacks = np.sort(os.listdir(os.path.join(suite2p_dir_base,setup,subject,FOV,'Z-stacks')))
+        for zstackname in available_z_stacks:
+            if '.tif' in zstackname:
+                Path(os.path.join(temp_movie_directory,zstackname[:-4])).mkdir(parents = True,exist_ok = True)
+                shutil.copyfile(os.path.join(suite2p_dir_base,setup,subject,FOV,'Z-stacks',zstackname),
+                                 os.path.join(temp_movie_directory,zstackname[:-4],zstackname))
+                continue
+            else:
+                zstackname = None
+        s2p_params['z_stack_name'] =zstackname
+        sp2_params_file = os.path.join(temp_movie_directory,'s2p_params.json')
+        #%
+        with open(sp2_params_file, "w") as data_file:
+            json.dump(s2p_params, data_file, indent=2)
+        #% 
+        if first_session_of_FOV: # first session needs a mean image to be generated
+            reference_movie_dir = os.path.join(temp_movie_directory,'_reference_image')
+            Path(reference_movie_dir).mkdir(parents = True,exist_ok = True)
+            os.chmod(reference_movie_dir, 0o777)
+            reference_movie_json = os.path.join(temp_movie_directory,'_reference_image','refimage_progress.json')
+            refimage_dict = {'ref_image_started':True,
+                             'ref_image_finished':False,
+                             'ref_image_started_time': str(time.time())}
+            with open(reference_movie_json, "w") as data_file:
+                json.dump(refimage_dict, data_file, indent=2)
+            trial_num_to_use = int(trial_number_for_mean_image)
+            #%
+            file_dict = np.load(os.path.join(temp_movie_directory,'copy_data.npy'),allow_pickle = True).tolist()
+            while len(file_dict['copied_files'])<trial_num_to_use:
+                
+                print('waiting for {} trials to be available for generating reference frame')
+                time.sleep(3)
+                file_dict = np.load(os.path.join(temp_movie_directory,'copy_data.npy'),allow_pickle = True).tolist()
+            cluster_command_list = ['cd /home/rozmar/Scripts/BCI_pipeline',
+                                    'python cluster_helper.py {} "\'{}\'" {}'.format('utils_imaging.generate_mean_image_from_trials',temp_movie_directory,trial_num_to_use)]
+            bash_command = r" && ".join(cluster_command_list)+ r" &"
+            os.system(bash_command)
+            first_session_of_FOV = False
+
+        #%% registering
+        copy_finished = False
+        all_files_registered = False
+        while not copy_finished or not all_files_registered:
+            file_dict = np.load(os.path.join(temp_movie_directory,'copy_data.npy'),allow_pickle = True).tolist()
+            copy_finished = file_dict['copy_finished']
+            processes_running = 0
+            movies_registered = 0
+            for file in file_dict['copied_files']:
+                if not os.path.exists(os.path.join(temp_movie_directory,'mean_image.npy')):
+                    print('no reference image!!')
+                    time.sleep(3)
+                    break
+                
+                dir_now = os.path.join(temp_movie_directory,file[:-4])
+                reg_json_file = os.path.join(temp_movie_directory,file[:-4],'reg_progress.json')
+                if 'reg_progress.json' in os.listdir(dir_now):
+                    with open(reg_json_file, "r") as read_file:
+                        reg_dict = json.load(read_file)
+                else:
+                    reg_dict = {'registration_started':False}
+                if 'registration_finished' in reg_dict.keys():
+                    if reg_dict['registration_finished']:
+                        movies_registered+=1
+                        continue
+                if reg_dict['registration_started']:
+                    processes_running+=1
+                    continue
+
+                cluster_command_list = ['cd /home/rozmar/Scripts/BCI_pipeline',
+                                        "python cluster_helper.py {} \"{}\" \"{}\"".format('utils_imaging.register_trial',temp_movie_directory,file)]
+                bash_command = r" && ".join(cluster_command_list)
+                if processes_running < max_process_num :
+                    print('starting {}'.format(file))
+                    reg_dict['registration_started'] = True
+                    with open(reg_json_file, "w") as data_file:
+                        json.dump(reg_dict, data_file, indent=2)
+                    bash_command = bash_command + '&'
+                    os.system(bash_command)
+                    processes_running+=1
+                    
+                else:
+                    break
+            if movies_registered == len(file_dict['copied_files']):
+                all_files_registered = True
+            else:
+                all_files_registered = False
+            print('{} running processes, {} files registered out of {}'.format(processes_running, movies_registered,len(file_dict['copied_files'])))
+            time.sleep(3)
         
         
         
