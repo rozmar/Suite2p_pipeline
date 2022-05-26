5c5d05c2b849215c9d04af7693d53a8d3803fbda
rozmar <qtyush@gmail.com>
2022-05-25 23:52:27 -0700

    always the previous session is the reference

--- register_main.py
+++ register_main.py
@@ -59,7 +59,7 @@ s2p_params = {'max_reg_shift':50, # microns
             'z_stack_name':'',
             'reference_session':''} # folder where the suite2p output is saved
 photostim_name_list = ['slm','stim','file','photo']
-reference_is_previous_session = False
+reference_is_previous_session = True
 skip_photostim_trials = True
 acceptable_z_range_for_binned_movie = 1
 trial_number_for_mean_image = 10
