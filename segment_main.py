e42755e8a8995e3a38b5b07279431e7616bf9dfc
rozmar <qtyush@gmail.com>
2022-06-15 22:59:38 -0700

    segmentation is now optional

--- segment_main.py
+++ segment_main.py
@@ -41,7 +41,12 @@ try:
     try:
         acceptable_z_range = int(sys.argv[9])#'Bergamo-2P-Photostim'
     except:
-        acceptable_z_range = None   
+        acceptable_z_range = None 
+    try:
+        segment_cells = sys.argv[10].lower() == 'true'#'Bergamo-2P-Photostim'
+    except:
+        segment_cells = True  
+    
 except:
     local_temp_dir = '/mnt/HDDS/Fast_disk_0/temp/'
     metadata_dir = '/mnt/Data/BCI_metadata/'
@@ -217,192 +222,192 @@ ax_contrast.legend()
 fig.savefig(os.path.join(FOV_dir,'XYZ_motion.pdf'), format="pdf")
 
 
-
-#%% concatenate binned movie
-max_binned_frame_num = 7000#after this it fills up the 120GB RAM
-binned_movie_concatenated = []
-ops_loaded=  False
-total_binned_frame_num = 0
-for session in sessions:
-    if 'z-stack' in session.lower() or '.' in session:
-        continue
-    if not ops_loaded:
-        ops = np.load(os.path.join(FOV_dir,session,'ops.npy'),allow_pickle = True).tolist()
-        ops_loaded = True
-    new_session_idx.append(trial_i)
-    if subject in blacklist_for_binned_movie.keys():
-        if session in blacklist_for_binned_movie[subject]:
-            print('session {} blacklisted, skipping from binned movie'.format(session))
+if segment_cells:
+    #%% concatenate binned movie
+    max_binned_frame_num = 7000#after this it fills up the 120GB RAM
+    binned_movie_concatenated = []
+    ops_loaded=  False
+    total_binned_frame_num = 0
+    for session in sessions:
+        if 'z-stack' in session.lower() or '.' in session:
             continue
-# =============================================================================
-#     with open(os.path.join(FOV_dir,session,'filelist.json')) as f:
-#         filelist_dict = json.load(f)
-#     zcorr = np.asarray(filelist_dict['zoff_list']).squeeze()
-# =============================================================================
-    zcorr = np.asarray(session_data_dict[session]['zoff']).squeeze()
-    max_zcorr_vals = np.max(zcorr,1)
-    min_zcorr_vals = np.min(zcorr,1)
-    contrast = max_zcorr_vals/min_zcorr_vals
-    median_z_session = np.median(np.argmax(zcorr,1))
-    
-    
-    if np.percentile(contrast,10)<minimum_contrast:
-        print('{} skipped due to low Z contrast'.format(session))
-        continue
-    if median_z_session<median_z_value-acceptable_z_range or median_z_session>median_z_value+acceptable_z_range :
-        print('{} skipped due to wrong Z position'.format(session))
-        continue
-    print('loading the binned movie of {}'.format(session))
-    mov = np.load(os.path.join(FOV_dir,session,'binned_movie.npy'))
-    binned_movie_concatenated.append(mov)
-    total_binned_frame_num += mov.shape[0]
-    
-binned_movie_concatenated = np.concatenate(binned_movie_concatenated)   
-#%
-t_step_size = int(np.ceil(binned_movie_concatenated.shape[0]/max_binned_frame_num))
-binned_movie_concatenated = binned_movie_concatenated[::t_step_size,:,:]
-#%% segment ROIs
-if use_cellpose:
-    ops['anatomical_only'] = True
-    ops['diameter'] = [10,10]
-else:
-    ops['anatomical_only'] = False
-ops['xrange'] = [0, ops['Lx']]
-ops['yrange'] = [0, ops['Ly']]
-ops, stat = detect(ops, classfile=None, mov = binned_movie_concatenated)
-stat_original = stat.copy()
-#%% cut off pixels that drift out of the FOV 
-if np.percentile(np.concatenate(yoff_list),5)<0:
-    ops['yrange'][0] = int(abs(np.percentile(np.concatenate(yoff_list),5)))
-if np.percentile(np.concatenate(xoff_list),5)<0:
-    ops['xrange'][0] = int(abs(np.percentile(np.concatenate(xoff_list),5)))
-    
-if np.percentile(np.concatenate(yoff_list),95)>0:
-    ops['yrange'][1] = ops['Ly']-int(abs(np.percentile(np.concatenate(yoff_list),95)))
-if np.percentile(np.concatenate(xoff_list),95)>0:
-    ops['xrange'][1] = ops['Lx']-int(abs(np.percentile(np.concatenate(xoff_list),95)))
-
-stat = []
-for s in stat_original:
-    needed_idx = (s['ypix']>ops['yrange'][0]) &(s['ypix']<ops['yrange'][1]) &(s['xpix']>ops['xrange'][0])  &(s['xpix']<ops['xrange'][1])
-    for k in s.keys():
-        try:
-            if len(s[k]) == len(needed_idx):
-                s[k] = s[k][needed_idx]
-        except:
-            pass
-    if sum(needed_idx)>1:
-        stat.append(s)
-    #break
-#%% calculate neuroil and cell masks
-cell_masks_, neuropil_masks_ = create_masks(ops, stat)
-#%% select good rois
-cutoff_pixel_num = [50, 300]
-cell_masks = []
-neuropil_masks = []
-rois = np.zeros_like(ops['meanImg'])
-rois_small = np.zeros_like(ops['meanImg'])
-rois_good = np.zeros_like(ops['meanImg'])
-npix_list = []
-npix_list_somacrop = []
-npix_list_somacrop_nooverlap = []
-stat_good = []
-cell_masks_rest = []
-neuropil_masks_rest = []
-stat_rest = []
-for i,(s,cell_mask,neuropil_mask) in enumerate(zip(stat,cell_masks_,neuropil_masks_)):
-    #neurpil_coord = np.unravel_index(s['neuropil_mask'],rois.shape)
-    rois[s['ypix'][s['soma_crop']==True],s['xpix'][s['soma_crop']==True]] += s['npix']#s['lam']/np.sum(s['lam'])
-    npix_list.append(s['npix'])
-    npix_list_somacrop.append(s['npix_soma'])
-    npix_list_somacrop_nooverlap.append(sum((s['overlap'] == False) & s['soma_crop']))
-    #rois[neurpil_coord[0],neurpil_coord[1]] = .5#cell['lam']/np.sum(cell['lam'])
-    idx = (s['soma_crop']==True) & (s['overlap']==False)
-    pixel_num = sum((s['overlap'] == False) & s['soma_crop'])
-    if pixel_num>=cutoff_pixel_num[0]  and pixel_num<=cutoff_pixel_num[1]:
-        rois_good[s['ypix'][idx],s['xpix'][idx]] =s['lam'][idx]/np.sum(s['lam'][idx])*sum(idx)
-        cell_masks.append(cell_mask)
-        neuropil_masks.append(neuropil_mask)
-        stat_good.append(s)
-    else:
-        rois_small[s['ypix'][idx],s['xpix'][idx]] =s['lam'][idx]/np.sum(s['lam'][idx])*sum(idx)
-        cell_masks_rest.append(cell_mask)
-        neuropil_masks_rest.append(neuropil_mask)
-        stat_rest.append(s)
-#%
-np.save(os.path.join(FOV_dir, 'stat.npy'), stat_good)
-scipy.io.savemat(
-    file_name=os.path.join(FOV_dir, 'stat.mat'),
-    mdict={
-        'stat': stat_good
-    }
-)
-np.save(os.path.join(FOV_dir, 'cell_masks.npy'), cell_masks)
-np.save(os.path.join(FOV_dir, 'neuropil_masks.npy'), neuropil_masks)
- 
-np.save(os.path.join(FOV_dir, 'stat_rest.npy'), stat_rest)
-np.save(os.path.join(FOV_dir, 'cell_masks_rest.npy'), cell_masks_rest)
-np.save(os.path.join(FOV_dir, 'neuropil_masks_rest.npy'), neuropil_masks_rest)
-       
-#%
-mean_image = np.mean(binned_movie_concatenated,0)
-max_image = np.max(binned_movie_concatenated,0)
-std_image = np.std(binned_movie_concatenated,0)
-#%
-np.save(os.path.join(FOV_dir, 'mean_image.npy'), mean_image)
-np.save(os.path.join(FOV_dir, 'max_image.npy'), max_image)
-np.save(os.path.join(FOV_dir, 'std_image.npy'), std_image)
+        if not ops_loaded:
+            ops = np.load(os.path.join(FOV_dir,session,'ops.npy'),allow_pickle = True).tolist()
+            ops_loaded = True
+        new_session_idx.append(trial_i)
+        if subject in blacklist_for_binned_movie.keys():
+            if session in blacklist_for_binned_movie[subject]:
+                print('session {} blacklisted, skipping from binned movie'.format(session))
+                continue
+    # =============================================================================
+    #     with open(os.path.join(FOV_dir,session,'filelist.json')) as f:
+    #         filelist_dict = json.load(f)
+    #     zcorr = np.asarray(filelist_dict['zoff_list']).squeeze()
+    # =============================================================================
+        zcorr = np.asarray(session_data_dict[session]['zoff']).squeeze()
+        max_zcorr_vals = np.max(zcorr,1)
+        min_zcorr_vals = np.min(zcorr,1)
+        contrast = max_zcorr_vals/min_zcorr_vals
+        median_z_session = np.median(np.argmax(zcorr,1))
         
-#%%
-fig = plt.figure(figsize = [20,20])
-ax_rois = fig.add_subplot(2,3,4)
-ax_rois.set_title('all ROIs (n = {})'.format(len(stat)))
-im = ax_rois.imshow(rois)
-im.set_clim([0,200])
-ax_rois_small = fig.add_subplot(2,3,5,sharex =ax_rois,sharey = ax_rois )
-ax_rois_small.set_title('small ROIs (n = {})'.format(len(stat_rest)))
-im2 = ax_rois_small.imshow(rois_small)
-im2.set_clim([0,np.percentile(rois_small[rois_small>0],95)])
-ax_rois_good = fig.add_subplot(2,3,6,sharex =ax_rois,sharey = ax_rois)
-ax_rois_good.set_title('cellular ROIs (n = {})'.format(len(stat_good)))
-im3 = ax_rois_good.imshow(rois_good)
-im3.set_clim([0,np.percentile(rois_good[rois_good>0],95)])
-
-fig_roisize = plt.figure(figsize = [20,20])
-ax_roisize = fig_roisize.add_subplot(3,1,1)
-ax_roisize.set_title('ROI sizes')
-ax_roisize_soma = fig_roisize.add_subplot(3,1,2,sharex = ax_roisize)
-ax_roisize_soma.set_title('somatic ROI sizes')
-ax_roisize_soma_nooverlap = fig_roisize.add_subplot(3,1,3,sharex = ax_roisize)
-ax_roisize_soma_nooverlap.set_title('somatic ROI sizes without overlaps')
-ax_roisize.hist(npix_list,np.arange(0,300,5))
-ax_roisize_soma.hist(npix_list_somacrop,np.arange(0,300,5))
-ax_roisize_soma_nooverlap.hist(npix_list_somacrop_nooverlap,np.arange(0,300,5))
-ax_roisize_soma_nooverlap.axvline(cutoff_pixel_num[0],color ='red')
-ax_roisize_soma_nooverlap.axvline(cutoff_pixel_num[1],color ='red', label = 'pixel size cutoff')
-ax_roisize_soma_nooverlap.set_xlabel('Pixel count')
-
-ax_meanimage = fig.add_subplot(2,3,1,sharex =ax_rois,sharey = ax_rois )
-ax_meanimage.set_title('mean image')
-im = ax_meanimage.imshow(mean_image)
-im.set_clim(np.percentile(mean_image.flatten(),[0,95]))
-ax_maximage = fig.add_subplot(2,3,2,sharex =ax_rois,sharey = ax_rois )
-ax_maximage.set_title('max projection')
-im = ax_maximage.imshow(max_image)
-im = ax_meanimage.imshow(mean_image)
-im.set_clim(np.percentile(max_image.flatten(),[0,95]))
-
-
-ax_stdimage = fig.add_subplot(2,3,3,sharex =ax_rois,sharey = ax_rois )
-ax_stdimage.set_title('std of image')
-im = ax_stdimage.imshow(std_image)
-im.set_clim(np.percentile(std_image.flatten(),[0,95]))
-if use_cellpose:
-    fig.savefig(os.path.join(FOV_dir,'ROIs_cellpose.pdf'), format="pdf")
-    fig_roisize.savefig(os.path.join(FOV_dir,'ROI_sizes_cellpose.pdf'), format="pdf")
-else:
-    fig.savefig(os.path.join(FOV_dir,'ROIs.pdf'), format="pdf")
-    fig_roisize.savefig(os.path.join(FOV_dir,'ROI_sizes.pdf'), format="pdf")
+        
+        if np.percentile(contrast,10)<minimum_contrast:
+            print('{} skipped due to low Z contrast'.format(session))
+            continue
+        if median_z_session<median_z_value-acceptable_z_range or median_z_session>median_z_value+acceptable_z_range :
+            print('{} skipped due to wrong Z position'.format(session))
+            continue
+        print('loading the binned movie of {}'.format(session))
+        mov = np.load(os.path.join(FOV_dir,session,'binned_movie.npy'))
+        binned_movie_concatenated.append(mov)
+        total_binned_frame_num += mov.shape[0]
+        
+    binned_movie_concatenated = np.concatenate(binned_movie_concatenated)   
+    #%
+    t_step_size = int(np.ceil(binned_movie_concatenated.shape[0]/max_binned_frame_num))
+    binned_movie_concatenated = binned_movie_concatenated[::t_step_size,:,:]
+    #%% segment ROIs
+    if use_cellpose:
+        ops['anatomical_only'] = True
+        ops['diameter'] = [10,10]
+    else:
+        ops['anatomical_only'] = False
+    ops['xrange'] = [0, ops['Lx']]
+    ops['yrange'] = [0, ops['Ly']]
+    ops, stat = detect(ops, classfile=None, mov = binned_movie_concatenated)
+    stat_original = stat.copy()
+    #%% cut off pixels that drift out of the FOV 
+    if np.percentile(np.concatenate(yoff_list),5)<0:
+        ops['yrange'][0] = int(abs(np.percentile(np.concatenate(yoff_list),5)))
+    if np.percentile(np.concatenate(xoff_list),5)<0:
+        ops['xrange'][0] = int(abs(np.percentile(np.concatenate(xoff_list),5)))
+        
+    if np.percentile(np.concatenate(yoff_list),95)>0:
+        ops['yrange'][1] = ops['Ly']-int(abs(np.percentile(np.concatenate(yoff_list),95)))
+    if np.percentile(np.concatenate(xoff_list),95)>0:
+        ops['xrange'][1] = ops['Lx']-int(abs(np.percentile(np.concatenate(xoff_list),95)))
+    
+    stat = []
+    for s in stat_original:
+        needed_idx = (s['ypix']>ops['yrange'][0]) &(s['ypix']<ops['yrange'][1]) &(s['xpix']>ops['xrange'][0])  &(s['xpix']<ops['xrange'][1])
+        for k in s.keys():
+            try:
+                if len(s[k]) == len(needed_idx):
+                    s[k] = s[k][needed_idx]
+            except:
+                pass
+        if sum(needed_idx)>1:
+            stat.append(s)
+        #break
+    #%% calculate neuroil and cell masks
+    cell_masks_, neuropil_masks_ = create_masks(ops, stat)
+    #%% select good rois
+    cutoff_pixel_num = [50, 300]
+    cell_masks = []
+    neuropil_masks = []
+    rois = np.zeros_like(ops['meanImg'])
+    rois_small = np.zeros_like(ops['meanImg'])
+    rois_good = np.zeros_like(ops['meanImg'])
+    npix_list = []
+    npix_list_somacrop = []
+    npix_list_somacrop_nooverlap = []
+    stat_good = []
+    cell_masks_rest = []
+    neuropil_masks_rest = []
+    stat_rest = []
+    for i,(s,cell_mask,neuropil_mask) in enumerate(zip(stat,cell_masks_,neuropil_masks_)):
+        #neurpil_coord = np.unravel_index(s['neuropil_mask'],rois.shape)
+        rois[s['ypix'][s['soma_crop']==True],s['xpix'][s['soma_crop']==True]] += s['npix']#s['lam']/np.sum(s['lam'])
+        npix_list.append(s['npix'])
+        npix_list_somacrop.append(s['npix_soma'])
+        npix_list_somacrop_nooverlap.append(sum((s['overlap'] == False) & s['soma_crop']))
+        #rois[neurpil_coord[0],neurpil_coord[1]] = .5#cell['lam']/np.sum(cell['lam'])
+        idx = (s['soma_crop']==True) & (s['overlap']==False)
+        pixel_num = sum((s['overlap'] == False) & s['soma_crop'])
+        if pixel_num>=cutoff_pixel_num[0]  and pixel_num<=cutoff_pixel_num[1]:
+            rois_good[s['ypix'][idx],s['xpix'][idx]] =s['lam'][idx]/np.sum(s['lam'][idx])*sum(idx)
+            cell_masks.append(cell_mask)
+            neuropil_masks.append(neuropil_mask)
+            stat_good.append(s)
+        else:
+            rois_small[s['ypix'][idx],s['xpix'][idx]] =s['lam'][idx]/np.sum(s['lam'][idx])*sum(idx)
+            cell_masks_rest.append(cell_mask)
+            neuropil_masks_rest.append(neuropil_mask)
+            stat_rest.append(s)
+    #%
+    np.save(os.path.join(FOV_dir, 'stat.npy'), stat_good)
+    scipy.io.savemat(
+        file_name=os.path.join(FOV_dir, 'stat.mat'),
+        mdict={
+            'stat': stat_good
+        }
+    )
+    np.save(os.path.join(FOV_dir, 'cell_masks.npy'), cell_masks)
+    np.save(os.path.join(FOV_dir, 'neuropil_masks.npy'), neuropil_masks)
+     
+    np.save(os.path.join(FOV_dir, 'stat_rest.npy'), stat_rest)
+    np.save(os.path.join(FOV_dir, 'cell_masks_rest.npy'), cell_masks_rest)
+    np.save(os.path.join(FOV_dir, 'neuropil_masks_rest.npy'), neuropil_masks_rest)
+           
+    #%
+    mean_image = np.mean(binned_movie_concatenated,0)
+    max_image = np.max(binned_movie_concatenated,0)
+    std_image = np.std(binned_movie_concatenated,0)
+    #%
+    np.save(os.path.join(FOV_dir, 'mean_image.npy'), mean_image)
+    np.save(os.path.join(FOV_dir, 'max_image.npy'), max_image)
+    np.save(os.path.join(FOV_dir, 'std_image.npy'), std_image)
+            
+    #%%
+    fig = plt.figure(figsize = [20,20])
+    ax_rois = fig.add_subplot(2,3,4)
+    ax_rois.set_title('all ROIs (n = {})'.format(len(stat)))
+    im = ax_rois.imshow(rois)
+    im.set_clim([0,200])
+    ax_rois_small = fig.add_subplot(2,3,5,sharex =ax_rois,sharey = ax_rois )
+    ax_rois_small.set_title('small ROIs (n = {})'.format(len(stat_rest)))
+    im2 = ax_rois_small.imshow(rois_small)
+    im2.set_clim([0,np.percentile(rois_small[rois_small>0],95)])
+    ax_rois_good = fig.add_subplot(2,3,6,sharex =ax_rois,sharey = ax_rois)
+    ax_rois_good.set_title('cellular ROIs (n = {})'.format(len(stat_good)))
+    im3 = ax_rois_good.imshow(rois_good)
+    im3.set_clim([0,np.percentile(rois_good[rois_good>0],95)])
+    
+    fig_roisize = plt.figure(figsize = [20,20])
+    ax_roisize = fig_roisize.add_subplot(3,1,1)
+    ax_roisize.set_title('ROI sizes')
+    ax_roisize_soma = fig_roisize.add_subplot(3,1,2,sharex = ax_roisize)
+    ax_roisize_soma.set_title('somatic ROI sizes')
+    ax_roisize_soma_nooverlap = fig_roisize.add_subplot(3,1,3,sharex = ax_roisize)
+    ax_roisize_soma_nooverlap.set_title('somatic ROI sizes without overlaps')
+    ax_roisize.hist(npix_list,np.arange(0,300,5))
+    ax_roisize_soma.hist(npix_list_somacrop,np.arange(0,300,5))
+    ax_roisize_soma_nooverlap.hist(npix_list_somacrop_nooverlap,np.arange(0,300,5))
+    ax_roisize_soma_nooverlap.axvline(cutoff_pixel_num[0],color ='red')
+    ax_roisize_soma_nooverlap.axvline(cutoff_pixel_num[1],color ='red', label = 'pixel size cutoff')
+    ax_roisize_soma_nooverlap.set_xlabel('Pixel count')
+    
+    ax_meanimage = fig.add_subplot(2,3,1,sharex =ax_rois,sharey = ax_rois )
+    ax_meanimage.set_title('mean image')
+    im = ax_meanimage.imshow(mean_image)
+    im.set_clim(np.percentile(mean_image.flatten(),[0,95]))
+    ax_maximage = fig.add_subplot(2,3,2,sharex =ax_rois,sharey = ax_rois )
+    ax_maximage.set_title('max projection')
+    im = ax_maximage.imshow(max_image)
+    im = ax_meanimage.imshow(mean_image)
+    im.set_clim(np.percentile(max_image.flatten(),[0,95]))
+    
+    
+    ax_stdimage = fig.add_subplot(2,3,3,sharex =ax_rois,sharey = ax_rois )
+    ax_stdimage.set_title('std of image')
+    im = ax_stdimage.imshow(std_image)
+    im.set_clim(np.percentile(std_image.flatten(),[0,95]))
+    if use_cellpose:
+        fig.savefig(os.path.join(FOV_dir,'ROIs_cellpose.pdf'), format="pdf")
+        fig_roisize.savefig(os.path.join(FOV_dir,'ROI_sizes_cellpose.pdf'), format="pdf")
+    else:
+        fig.savefig(os.path.join(FOV_dir,'ROIs.pdf'), format="pdf")
+        fig_roisize.savefig(os.path.join(FOV_dir,'ROI_sizes.pdf'), format="pdf")
 
 #%% generate MEGATIFF
 
