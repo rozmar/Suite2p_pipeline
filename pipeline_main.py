54c16f489f470f5baaa381669254f50ac07856c5
rozmar <qtyush@gmail.com>
2022-07-26 16:54:50 -0700

    extraction of photostim and rest of the ROIs

--- pipeline_main.py
+++ pipeline_main.py
@@ -13,6 +13,8 @@ resegment_cells = 'true' in sys.argv[5].lower()
 correlte_z_stacks = 'true' in sys.argv[6].lower()
 overwrite_export = 'true' in sys.argv[7].lower()
 register_photostim = 'true' in sys.argv[8].lower()
+export_photostim = 'true' in sys.argv[9].lower()
+export_photostim_apical_dendrites = 'true' in sys.argv[9].lower()
 
 # - HARD-CODED VARIABLES FOR GOOGLE CLOUD
 local_temp_dir = '/home/jupyter/temp/' 
@@ -24,26 +26,22 @@ setup = 'Bergamo-2P-Photostim'
 save_path = "/home/jupyter/bucket/Data/Calcium_imaging/sessionwise_tba"
 # - HARD-CODED VARIABLES FOR GOOGLE CLOUD
 if register_z_stacks:
-# =============================================================================
-#     try:
-# =============================================================================
-    register.register_z_stacks(local_temp_dir = local_temp_dir,
-                              metadata_dir = metadata_dir,
-                              raw_scanimage_dir_base =raw_scanimage_dir_base,
-                              suite2p_dir_base = suite2p_dir_base,
-                              subject_ = subject,
-                              setup = setup)
-# =============================================================================
-#     except:
-#         print('could not register z-stack, trying averaging')
-#         register.register_z_stacks(local_temp_dir = local_temp_dir,
-#                                   metadata_dir = metadata_dir,
-#                                   raw_scanimage_dir_base =raw_scanimage_dir_base,
-#                                   suite2p_dir_base = suite2p_dir_base,
-#                                   subject_ = subject,
-#                                   setup = setup,
-#                                   method = 'averaging')
-# =============================================================================
+    try:
+        register.register_z_stacks(local_temp_dir = local_temp_dir,
+                                  metadata_dir = metadata_dir,
+                                  raw_scanimage_dir_base =raw_scanimage_dir_base,
+                                  suite2p_dir_base = suite2p_dir_base,
+                                  subject_ = subject,
+                                  setup = setup)
+    except:
+        print('could not register z-stack, trying averaging')
+        register.register_z_stacks(local_temp_dir = local_temp_dir,
+                                  metadata_dir = metadata_dir,
+                                  raw_scanimage_dir_base =raw_scanimage_dir_base,
+                                  suite2p_dir_base = suite2p_dir_base,
+                                  subject_ = subject,
+                                  setup = setup,
+                                  method = 'averaging')
 
 if regiter_sessions:
     register.register_session(local_temp_dir = local_temp_dir,
@@ -78,6 +76,9 @@ extract.extract_traces(local_temp_dir = local_temp_dir,
                       fov = fov,
                       overwrite = overwrite_export)
 
+
+    
+
 BCI_analysis.io_suite2p.suite2p_to_npy(os.path.join(suite2p_dir_base,setup), 
                                        os.path.join(raw_scanimage_dir_base,setup), 
                                        os.path.join(bpod_path,setup),
@@ -96,3 +97,22 @@ if register_photostim:
                               max_process_num = 4,
                               batch_size = 50,
                               FOV_needed = fov)
+
+
+if export_photostim:
+    if export_photostim_apical_dendrites:
+        roi_types = ['','_rest']
+    else:
+        roi_types= ['']
+        
+    extract.extract_traces(local_temp_dir = local_temp_dir,
+                          metadata_dir = metadata_dir,
+                          raw_scanimage_dir_base =raw_scanimage_dir_base,
+                          suite2p_dir_base = suite2p_dir_base,
+                          bpod_path = bpod_path,
+                          subject = subject,
+                          setup = setup,
+                          fov = fov,
+                          overwrite = overwrite_export,
+                          roi_types = roi_types,
+                          photostim = True)
